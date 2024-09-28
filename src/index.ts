8b641fde85bd564dff0ab956d393c5431949885d
Nexus <nicolasestevaolima104@gmail.com>
2024-09-28 06:25:00 -0300

    feat: add global init vars to diseact

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-Object.assign(global, { DISEACT_COMMAND_MAP: new Map() });
+Object.assign(global, { 
+    DISEACT_COMMAND_MAP: new Map(), 
+    DISEACT_CURRENT_STATE_HOOK: { component: undefined, index: 0 } 
+});
 
 import './structures';
 import './client';
