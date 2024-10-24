9b1e420a9d63325cb63f7cb07611afa7a26f5ec8
Nexus <nicolasestevaolima104@gmail.com>
2024-10-24 13:21:04 -0300

    feat: add client declaration

--- src/structures/inject.decorator.ts
+++ src/structures/inject.decorator.ts
@@ -1,5 +1,7 @@
 import { Client } from "discord.js";
 
+declare const client: Client;
+
 function inject(constructor: new (client: Client) => unknown, context: ClassDecoratorContext) {
     if (context.kind != 'class') {
         throw new Error("This Decorator only can be used on Class");
