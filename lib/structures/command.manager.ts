204cadd54f025f872c34661b0910a8659197da1f
Nexus <nicolasestevaolima104@gmail.com>
2024-11-26 11:18:57 -0300

    feat: separe structures of client helper

---
+++ lib/structures/command.manager.ts
@@ -0,0 +1,16 @@
+import Diseact from 'diseact';
+import { Client } from "discord.js";
+
+export class CommandManager {
+    listen() {
+        this.client.on('interactionCreate', interaction => {
+            if(interaction.isCommand() || interaction.isAutocomplete()) {
+                // TODO: make Diseact autocomplete 
+                // @ts-ignore
+                Diseact.CommandInteractionExecutor(interaction);
+            }
+        })
+    }
+
+    constructor(private client: Client) {}
+}
--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,46 +1,14 @@
-import Diseact from 'diseact';
-import Discord from 'discord.js';
+import { FlameClient } from '@flame-oh/core';
 import _commands from './commands';
 
 const { TOKEN } = process.env;
 
 void async function () {
-    const module = await _commands();
+    const module = await _commands;
     await Promise.all(Object.values(module));
 }();
 
-class CommandManager {
-    listen() {
-        this.client.on('interactionCreate', interaction => {
-            if(interaction.isCommand() || interaction.isAutocomplete()) {
-                // TODO: make Diseact autocomplete 
-                Diseact.CommandInteractionExecutor(interaction);
-            }
-        })
-    }
-
-    constructor(client) {}
-}
-
-class Client extends Discord.Client {
-    commands = new CommandManager(this) 
-    
-    constructor(options) {
-        super(options);
-
-        this.commands.listen();
-    }
-
-    async onReady() {
-        const { promise, resolve } = Promise.withResolvers();
-
-        this.once('ready', resolve);
-
-        return await promise;
-    }
-}
-
-const client = new Client({ intents: INTENTS });
+const client = new FlameClient({ intents: INTENTS });
 
 MANAGERS
 
