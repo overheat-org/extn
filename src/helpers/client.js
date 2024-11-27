64984c5fbbdc5d4cded71675cb7b96fe2a15761c
Nexus <nicolasestevaolima104@gmail.com>
2024-11-27 19:05:33 -0300

    feat: removed unnecessary function

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,14 +1,9 @@
 import { FlameClient } from '@flame-oh/core';
-import _commands from './commands';
 
 const { TOKEN } = process.env;
 
-void async function () {
-    const module = await _commands;
-    await Promise.all(Object.values(module));
-}();
-
 const client = new FlameClient({ intents: INTENTS });
+import('./commands');
 
 MANAGERS
 
