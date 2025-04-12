e9dde1757e6b3176ab064d224c512d51c1a1d072
Nexus <nicolasestevaolima104@gmail.com>
2025-04-12 18:26:40 -0300

    chore: debugger opts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig({
         noExternal: true,
     },
     build: {
+        sourcemap: true,
         lib: {
             entry: [
                 path.resolve(__dirname, 'src/lib/index.ts'),
